36da1f02f1986bc635a4a964d6a6a7f2508262f3
John <johnandersenpdx@gmail.com>
2025-03-15 16:25:10 -0600

    feat(kro): kubernetes resoruce graph defintion
    Signed-off-by: John <johnandersenpdx@gmail.com>

--- script.sh
+++ script.sh
@@ -14,4 +14,7 @@ docker-compose exec client conjur variable set -i BotApp/secretVar -v ${secretVa
 docker exec -e API_KEY=$(cat my_app_data | jq -r '.created_roles."myConjurAccount:host:BotApp/myDemoApp".api_key') bot_app sh -c 'curl -sk -d "${API_KEY}" https://proxy/authn/myConjurAccount/host%2FBotApp%2FmyDemoApp/authenticate | tee /tmp/conjur_token' | yq -P
 docker exec bot_app /tmp/program.sh
 echo $secretVal
+echo TODO 'https://docs.cyberark.com/conjur-enterprise/latest/en/content/integrations/k8s-ocp/k8s-jwt-authn.htm?tocpath=Integrations%7COpenShift%252FKubernetes%7CAuthenticate%20OpenShift%252FKubernetes%7C_____2'
+echo TODO 'https://docs.cyberark.com/conjur-enterprise/latest/en/content/deployment/dap/dap-followers-lp.htm'
+echo TODO 'https://docs.cyberark.com/conjur-enterprise/latest/en/content/integrations/k8s-ocp/k8s-conjfollower.htm'
 echo TODO 'https://docs.cyberark.com/conjur-open-source/latest/en/content/integrations/k8s-ocp/k8s-app-identity-jwt.htm?tocpath=Integrations%7COpenShift%252FKubernetes%7CSet%20up%20Kubernetes%20authentication%7CJWT-based%20Kubernetes%20authentication%7C_____1'
